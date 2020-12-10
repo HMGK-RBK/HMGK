cb7ab9f0316641e6248778c61744975d228a6854
Hbib24 <habib.bekir@gmail.com>
2020-12-10 11:45:36 +0100

    init

--- baftekhome/react-client/src/index.jsx
+++ baftekhome/react-client/src/index.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/home.jsx";
 
 class App extends React.Component {
-    constructor(props) {
-        super()
-        this.state = {
-        }
-
-    }
-    render() {
-        return (
-            <h1>hello</h1>
-        )
-    }
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <Home />;
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
