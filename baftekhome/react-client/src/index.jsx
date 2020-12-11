6b1d4a0f07d1bdba15b879dccc5407a0cb2e73c3
ghofranemessaoudi <messaoudighofrane2@gmail.com>
2020-12-11 16:52:19 +0100

    authentication-done

--- baftekhome/react-client/src/index.jsx
+++ baftekhome/react-client/src/index.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./components/home.jsx";
+import SignUp from "./components/signUp.jsx"
+import Login from "./components/login.jsx"
 
 class App extends React.Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+     
+    };
   }
   render() {
-    return <Home />;
+    return( 
+      <div>
+  <Home />
+   <SignUp/>
+   <Login/>
+      </div>
+  
+    )
   }
 }
 
--- baftekhome/server/index.js
+++ baftekhome/server/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const port = 3000;
 
 const Home = require("../database/homes.js");
+const Users = require("../database/users.js");
 
 mongoose.connect(
   "mongodb+srv://hbib:hbib@cluster0.m3m3t.mongodb.net/BaftekHome?retryWrites=true&w=majority",
@@ -17,6 +19,36 @@ mongoose.connect(
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./react-client/dist"));
+
+app.post("/api/newuser", (req, res) => {
+  const saltRounds = 10;
+  var obj = req.body;
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) {
+      throw err;
+    }
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+      obj.password = hash;
+      Users.create(obj);
+    });
+  });
+});
+
+app.post("/api/users", (req, res) => {
+  Users.find({ email: req.body.email }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    bcrypt.compare(req.body.password, docs[0].password, function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+      if (result) {
+        res.send(docs);
+      }
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
